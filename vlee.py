b45bfd03ea70a1ae11251d460682b943ed488929
Dialo Diop <ddiop@umd.edu>
2022-03-30 11:45:44 -0400

    added another print method response

--- vlee.py
+++ vlee.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-""" Testing file for Exercise Assignment""" 
-=======
 """ Testing file for Exercise Assignment"""
 print("Hey, it's INST 326!")
 print("How's it going?")
->>>>>>> 35280be9158f3a9b135c31c19960ad2cd9c69ba5
+print("Doing well, How about you?")
